bfd8c48019d0cb22f242fc2c9bda28e9983d3a9a
Alexander Tonkhonoev <a.tonkhonoev@gmail.com>
2020-12-10 02:45:32 +0700

    Fixed alertController

--- ProjectList/ProjectList/Common/Extensions/UIViewController/UIViewController+ErrorAlert.swift
+++ ProjectList/ProjectList/Common/Extensions/UIViewController/UIViewController+ErrorAlert.swift
@@ -11,6 +11,8 @@ extension UIViewController {
     
     func showErrorInAlert(errorMessage: String) {
         let alertVC = UIAlertController(title: "Ошибка", message: errorMessage, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Печаль", style: .cancel)
+        alertVC.addAction(alertAction)
         present(alertVC, animated: true, completion: nil)
     }
     
